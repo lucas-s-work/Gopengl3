28a82ef77dfbb4c0a5a88619cb0b888041d6a3b3
Lucas Spencer <lucas@lucass-mbp-3.home>
2022-02-13 13:16:09 +0000

    Detect key tapping

--- graphics/gl/window.go
+++ graphics/gl/window.go
@@ -1,7 +1,10 @@
 package gl
 
 import (
+	"sync"
+
 	"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/go-gl/mathgl/mgl32"
 )
 
 type Window struct {
@@ -12,8 +15,12 @@ type Window struct {
 
 type InputInfo struct {
 	Mx, My     int
-	M1, M2, M3 bool
-	KeyMap     map[string]bool
+	M1, M2, M3 key
+	KeyMap     map[string]key
+}
+
+type key struct {
+	KeyState, LastKeyState bool
 }
 
 func CreateWindow(width, height int, name string) (*Window, error) {
@@ -52,7 +59,8 @@ func (w *Window) SwapBuffers() {
 
 func (w *Window) PollInput() {
 	glfw.PollEvents()
-	updateKeys(w.glWindow)
+	w.updateKeys()
+	w.updateMouse()
 }
 
 func (w *Window) ShouldClose() bool {
@@ -78,4 +86,143 @@ func (w *Window) PixToScreen(x, y int) (float32, float32) {
 	sy--
 
 	return sx, sy
+}
+
+var (
+	keyMap   = map[glfw.Key]*key{}
+	mouseMap = map[glfw.MouseButton]*key{}
+	keyMut   = sync.Mutex{}
+	mousePos = mgl32.Vec2{}
+
+	keys = []glfw.Key{
+		glfw.KeyW,
+		glfw.KeyD,
+		glfw.KeyS,
+		glfw.KeyA,
+		glfw.KeyUp,
+		glfw.KeyDown,
+		glfw.KeyRight,
+		glfw.KeyLeft,
+	}
+
+	mouseButtons = []glfw.MouseButton{
+		glfw.MouseButton1,
+		glfw.MouseButton2,
+		glfw.MouseButton3,
+	}
+)
+
+func (w *Window) updateKeys() {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	for _, k := range keys {
+		w.checkKeyState(k)
+	}
+}
+
+func (w *Window) checkKeyState(glfwKey glfw.Key) {
+	if k, ok := keyMap[glfwKey]; ok {
+		k.KeyState = w.glWindow.GetKey(glfwKey) == glfw.Press
+	} else {
+		keyMap[glfwKey] = &key{
+			KeyState: w.glWindow.GetKey(glfwKey) == glfw.Press,
+		}
+	}
+}
+
+func (w *Window) checkMouseState(glfwButton glfw.MouseButton) {
+	if k, ok := mouseMap[glfwButton]; ok {
+		k.KeyState = w.glWindow.GetMouseButton(glfwButton) == glfw.Press
+	} else {
+		mouseMap[glfwButton] = &key{
+			KeyState: w.glWindow.GetMouseButton(glfwButton) == glfw.Press,
+		}
+	}
+}
+
+func (w *Window) updateMouse() {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	window := w.glWindow
+
+	x, y := window.GetCursorPos()
+	mousePos = mgl32.Vec2{float32(x), w.Height - float32(y)}
+
+	for _, b := range mouseButtons {
+		w.checkMouseState(b)
+	}
+}
+
+func GetMouseInfo() (mgl32.Vec2, map[glfw.MouseButton]*key) {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	return mousePos, mouseMap
+}
+
+func CheckKeyPressed(key glfw.Key) bool {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	if keyMap[key] == nil {
+		return false
+	}
+
+	return keyMap[key].KeyState
+}
+
+func CheckKeyTapped(key glfw.Key) bool {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	s := keyMap[key]
+	if s == nil {
+		return false
+	}
+
+	tapped := s.KeyState && (s.KeyState != s.LastKeyState)
+	s.LastKeyState = s.KeyState
+	return tapped
+}
+
+func CheckMouseTapped(button glfw.MouseButton) bool {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	s := mouseMap[button]
+	if s == nil {
+		return false
+	}
+
+	tapped := s.KeyState && (s.KeyState != s.LastKeyState)
+	s.LastKeyState = s.KeyState
+	return tapped
+}
+
+func CheckKeysPressed(keys []glfw.Key) []bool {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	out := make([]bool, len(keys))
+	for i, k := range keys {
+		out[i] = keyMap[k].KeyState
+	}
+
+	return out
+}
+
+func CheckKeyComboPressed(keys []glfw.Key) bool {
+	keyMut.Lock()
+	defer keyMut.Unlock()
+
+	for _, v := range keys {
+		k, ok := keyMap[v]
+		if !k.KeyState || !ok {
+			return false
+		}
+	}
+
+	return true
 }
